3a0466a0f078e2659dd142e132f5b5546648c401
David Taylor <dtaylor@wavetronix.com>
2015-05-14 17:07:43 -0600

    added another class for mod

---
+++ src/main/java/com/sgs/mcma/mod/ClientChatHandler.java
@@ -0,0 +1,22 @@
+package com.sgs.mcma.mod;
+
+import net.minecraftforge.client.event.ClientChatReceivedEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class ClientChatHandler {
+	BaseMod base;
+	public ClientChatHandler(BaseMod base)
+	{
+		this.base = base;
+	}
+	@SideOnly(Side.CLIENT)
+	@SubscribeEvent
+	public void onChatMessageReceived(ClientChatReceivedEvent event) {
+		base.logInfo("ClientChatHandler received ClientChatReceivedEvent");
+		base.logInfo("ClientChatHandler handled ClientChatReceivedEvent");
+		event.get
+	}
+}
+ 
